bdc433484a442712c94f326d457604777e64103c
Andre Mattis <amattis@taskstream.com>
2017-02-16 14:37:06 -0500

    Added HTML

--- app/features/booking/components/Vacation/Vacation.js
+++ app/features/booking/components/Vacation/Vacation.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-
+import '../../../../css/global.css';
 
 import {
   getUserDaysLeft
@@ -38,6 +38,37 @@ export default class Vacation extends Component {
     const { dispatch } = this.props;
     return (
       <div>
+        <header>
+          <h1></h1>
+          <h2>Employee Vacation Request Form</h2>
+        </header>
+        <main>
+          <div class="alt-font balance">@mike, your vacation balance is <span class="bold">15 days</span></div>
+          <article class="approved">
+            <h3>Approved requests: <span class="bold">0</span></h3>
+          </article>
+          <article class="pending">
+            <h3>Pending requests: <span class="bold">5 days</span></h3>
+            <table>
+              <thead>
+              <tr>
+                <th>Dates</th>
+                <th class="days">Requested Number of Days</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td>3/6/2017 - 3/8/2017</td>
+                <td>3</td>
+              </tr>
+              <tr>
+                <td>5/22/2017 - 5/23/2017</td>
+                <td>2</td>
+              </tr>
+              </tbody>
+            </table>
+          </article>
+        </main>
         <h4>
         days left
         </h4>
